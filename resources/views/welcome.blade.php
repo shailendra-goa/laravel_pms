81de63ab436c6d6b8996a8ac5eaf7c918f7d871c
mshailendra <shailendra_m2@yahoo.com>
2021-03-19 22:33:16 +0530

    changed welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Shailendra Laravel
                 </div>
 
                 <div class="links">
